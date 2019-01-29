3b26ae8659f696e12656bf0faa6580279e5ba09e
juaneloy19 <juaneloyfuentes@gmail.com>
2019-01-28 21:58:27 -0500

    Create parameters.v

---
+++ RTL/parameters.v
@@ -0,0 +1,3 @@
+parameter max_cache_width =  8 ;//256
+parameter max_r = 2; //Values from c-model will be restricted to 0 -> 3 for now reference control block for mapping
+parameter cur_addr_max = 4; 
