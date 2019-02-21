610676e9cf2bbce84d70658882290b89bf471c08
daves-devel <d.chakkuthara@gmail.com>
2019-02-21 03:24:57 -0500

    first revision of Visualization tool, can overlay colormap on video

---
+++ A2/VisualizationTool/VisualizationToolMVs.m
@@ -0,0 +1,25 @@
+
+v = VideoReader('ffmpeg-20190219-ff03418-win64-static\output.mp4');
+
+height = v.height;
+width = v.width;
+z = zeros(height, width);
+for r = 1:height
+    for c = 1:width
+        z(r,c) = r+c;
+    end
+end
+
+fig = figure;
+colormap('hot');
+im = imagesc(z);
+colorbar;
+
+currAxes = axes;
+while hasFrame(v)
+    vidFrame = readFrame(v);
+    imv = image(vidFrame, 'Parent', currAxes);
+    imv.AlphaData = 0.5;
+    currAxes.Visible = 'off';
+    pause(1/v.FrameRate);
+end
